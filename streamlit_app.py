16ea5f8e40b41fc0dd55e2487d3622f9e382f82d
Gurnoor Singh Saini <92860451+Sainigurnoor511@users.noreply.github.com>
2024-08-11 23:20:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ user_input = st.text_input("Please enter your review >>: ")
 
 specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
 
-
-if (specific_model(user_input)[0]['label']) == "POS" :
+if (specific_model(user_input)[0]['label']) == 0 :
+    st.write(" ")
+elif (specific_model(user_input)[0]['label']) == "POS" :
     st.write("# Positive")
 elif (specific_model(user_input)[0]['label']) == "NEU" :
     st.write("# Neutral")
