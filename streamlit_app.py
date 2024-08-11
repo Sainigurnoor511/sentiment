8ee3e13ef739d7059653243c45a87b10169851d0
Gurnoor Singh Saini <92860451+Sainigurnoor511@users.noreply.github.com>
2024-08-11 11:14:12 +0000

    Sentiment

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("😊 😐 😠  Sentiment Analysis")
+st.title("Sentiment Analysis")
+st.write("An app to analyse your reviews")
+
+
+user_input = st.text_input("Please enter your review >>: ")
+
+specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+
+
+if (specific_model(user_input)[0]['label']) == "POS" :
+    print("Positive")
+elif (specific_model(user_input)[0]['label']) == "NEU" :
+    print("Neutral")
+elif (specific_model(user_input)[0]['label']) == "NEG" :
+    print("Negative")
+else :
+    print("Error")
+
+
+if score == 0:
+    st.write(" ")
+elif score["neg"] != 0:
+    st.write("# Negative")
+elif score["pos"] != 0:
+    st.write("# Positive")
